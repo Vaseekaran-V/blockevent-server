5676f9e4d1f598837483bb4fe535e2e8b4181d66
mithushancj <mithushancj@gmail.com>
2019-04-19 02:10:13 +0530

    serverless deployment configs

--- src/dao/index.ts
+++ src/dao/index.ts
@@ -5,10 +5,10 @@ dotenv.config();
 const AWS = require("aws-sdk");
 
 let awsConfig = {
-    "region": process.env.AWS_REGION,
-    "endpoint": process.env.AWS_ENDPOINT,
-    "accessKeyId": process.env.AWS_ACCESSKEYID,
-    "secretAccessKey":process.env.AWS_SECRETACCESSKEY,
+    "region": 'us-east-1',
+    "endpoint": 'http://dynamodb.us-east-1.amazonaws.com',
+    "accessKeyId": 'AKIARSUODZUM43LMWTP4',
+    "secretAccessKey": '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',
 };
 
 AWS.config.update(awsConfig);
