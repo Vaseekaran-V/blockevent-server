af34c56258a246be26bbd1b1e2a8447618b932e0
mithushancj <mithushancj@gmail.com>
2019-04-19 02:10:22 +0530

    serverless deployment configs

---
+++ src/app.ts
@@ -0,0 +1,61 @@
+import * as bodyParser from "body-parser";
+import express = require("express");
+import * as apiRoutes from "./routes/routes";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { eventContext } from 'aws-serverless-express/middleware';
+const helmet = require('helmet')
+
+dotenv.config();
+// Create a new express application instance
+export function configureApp() {
+  const app: express.Application = express();
+  // The port the express app will listen on
+  const port = process.env.PORT || 7000;
+  app.use(helmet())
+
+  app.use(eventContext());
+
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  app.use(cors({
+    // "origin": "https://blockevent.tk",
+    // "origin": "http://localhost:4200",    
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }));
+
+  // app.use(bodyParser);
+  // app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json({ limit: '50mb' }));
+
+
+  // app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.use("/api", apiRoutes.router);
+
+  // Serve the application at the given port
+  // app.listen(port, () => {
+  //   // Success callback
+  //   // tslint:disable-next-line:no-console
+  //   console.log(`Listening at http://localhost:${port}/`);
+  // });
+
+  // cron.schedule("* */1 * * *", function() {
+  //   const controller = new requestHandler.matchData;
+  //     new Promise((resolve, reject) => {
+  //         controller.matchRequestV3();
+
+  //           // console.log("THE CRON HAS MATCHED REQUESTS!!!");
+
+
+  //     })
+  // });
+
+  return app;
+}
